8d231bd5c0c8c80560381d8c942e51ea1ff2e4d6
rebrindanard <rebrin53@gmail.com>
2022-08-30 08:29:36 -0500

    Initial commit

---
+++ src/Alumno.java
@@ -0,0 +1,22 @@
+import com.sun.nio.sctp.AbstractNotificationHandler;
+
+public class Alumno {
+    String Nombre;
+    String APaterno;
+    String AMaterno;
+    Integer NC;
+
+    //const
+    public Alumno(String Nombre, String paterno, String materno,Integer nc){
+        this.Nombre=Nombre;
+        APaterno=paterno;
+        AMaterno=materno;
+        NC=nc;
+    }
+
+    @Override
+    public String toString() {
+        return "NC:"+NC+" Nombre:"+Nombre+" "+APaterno+" "+AMaterno;
+    }
+
+}
---
+++ src/TestAlumno.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class TestAlumno {
+    public static void main(String[] args) {
+        Alumno a1 = new Alumno("rebrin", "rubalcava", "lopez", 1);
+        Alumno a2 = new Alumno("Esteban", "Sanchez", "Garcia", 2);
+        Alumno a3 = new Alumno("Miguel de Jesus", "Juarez", "Chavez", 3);
+        Alumno a4 = new Alumno("Miguel Angel", "Juarez", "Chavez", 4 );
+        /*Alumno[] alumnos=new Alumno[4];
+        alumnos[0]=a1;
+        alumnos[1]=a2;
+        alumnos[2]=a3;
+        alumnos[3]=a4;*/
+        ArrayList<Alumno>Alumnos=new ArrayList<>();
+        Alumnos.add(a1);
+        Alumnos.add(a2);
+        Alumnos.add(a3);
+        Alumnos.add(a4);
+        Alumnos.add(new Alumno("Carlos","Campos","Charicata",2022));
+        for (int i=0;i<Alumnos.size();i++) {
+            System.out.println(Alumnos.get(i));
+            
+        }
+
+       /* for (int i = 0; i < alumnos.length; i++) {
+            System.out.println(alumnos[i]);
+        }*/
+    }
+}
+
