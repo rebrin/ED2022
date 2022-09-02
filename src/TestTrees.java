cbb28d99e10f0916d271f02d220f8c264840c4e4
rebrindanard <rebrin53@gmail.com>
2022-09-02 08:31:25 -0500

    Tree y recorrido con Iterator y For

---
+++ src/TestTrees.java
@@ -0,0 +1,40 @@
+import com.sun.source.tree.Tree;
+
+import javax.print.attribute.standard.MediaSize;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.TreeSet;
+
+public class TestTrees {
+    public static void main(String[] args) {
+        Random r=new Random();
+        ArrayList<Integer>ListaEnteros=new ArrayList<>();
+        TreeSet<Integer>TreeEnteros=new TreeSet<>();
+        for (int i = 0; i < 1000000; i++) {
+            Integer NAlea=r.nextInt(1000000);
+            ListaEnteros.add(NAlea);
+            TreeEnteros.add(NAlea);
+        }
+        ListaEnteros.add(1000001);
+        TreeEnteros.add(1000001);
+
+        //System.out.println(ListaEnteros);
+        //System.out.println(TreeEnteros);
+
+        double ti=System.currentTimeMillis();
+        System.out.println(TreeEnteros.contains(1000001));
+        System.out.println(System.currentTimeMillis()-ti);
+        /* formas de iterar
+        for (Integer ele:TreeEnteros) {
+            System.out.println(ele);
+        }*/
+
+        Iterator<Integer>it=ListaEnteros.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+
+    }
+}
